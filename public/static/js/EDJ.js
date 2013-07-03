f2bcefd468b9eb4c1152bea458a21f99f0c7d1d8
Robin Rendle <robinjrendle@gmail.com>
2013-07-03 13:55:00 +0100

    tidied up the dropdown

--- public/static/js/EDJ.js
+++ public/static/js/EDJ.js
@@ -3,8 +3,18 @@ $(document).ready(function(){
 
 // Mobile navigation drop down
     $('.link__mobile-menu').on('click', function(e){
+    	var $mobile_link = $('link__mobile-menu'),
+			$icon_text = $('.icon--menu');
+
         e.preventDefault();
         $('#nav_pri').toggleClass('menu--active');
+
+        console.log($icon_text);
+        $icon_text.text('navigateup');
+        $icon_text.removeClass('navigatedown');
+        $icon_text.addClass('navigateup');
+
+        $mobile_link.find('.ss').text('navigateup');
         $('.banner-nav').slideToggle();
     });
 });

8d7e5c11a5c5dab18b9791df40b7aea64b19b35d
Robin Rendle <robinjrendle@gmail.com>
2013-07-03 16:54:49 +0100

    fixed the toggle and the icon position

--- public/static/js/EDJ.js
+++ public/static/js/EDJ.js
@@ -3,18 +3,28 @@ $(document).ready(function(){
 
 // Mobile navigation drop down
     $('.link__mobile-menu').on('click', function(e){
-    	var $mobile_link = $('link__mobile-menu'),
-			$icon_text = $('.icon--menu');
+        var $mobile_link = $('link__mobile-menu'),
+            $icon_text = $('.icon--menu');
 
         e.preventDefault();
         $('#nav_pri').toggleClass('menu--active');
-
-        console.log($icon_text);
-        $icon_text.text('navigateup');
+        $('.banner-nav').slideToggle();
         $icon_text.removeClass('navigatedown');
-        $icon_text.addClass('navigateup');
+        $icon_text.removeClass('ss-navigatedown');
+        $icon_text.toggleClass('navigateup');
+        $icon_text.toggleClass('ss-navigateup');
+
+
+
+        if ($('#nav_pri').hasClass('menu--active')) {
+            $icon_text.text('navigateup');
+        }
+
+        else {
+            $icon_text.text('navigatedown');
+            $icon_text.addClass('navigatedown');
+        }
 
         $mobile_link.find('.ss').text('navigateup');
-        $('.banner-nav').slideToggle();
     });
 });

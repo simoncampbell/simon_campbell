50782597f79f25bf687d7bfa43dfff1808d187a2
Robin Rendle <robinjrendle@gmail.com>
2013-12-03 15:28:51 +0000

    Fixed mixins, removed toolkit

--- public/config.rb
+++ public/config.rb
@@ -1,25 +1,25 @@
 # Require any additional compass plugins here.
-require 'toolkit';
+
 require 'breakpoint';
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/static/css"
-sass_dir = "public/static/scss"
-images_dir = "public/static/images"
-javascripts_dir = "public/static/js"
-fonts_dir = "public/static/fonts"
+css_dir = "static/css"
+sass_dir = "static/scss"
+images_dir = "static/images"
+javascripts_dir = "static/js"
+fonts_dir = "static/fonts"
 
 output_style = :nested
 encoding = "utf-8"
 environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-color_output = false
+color_output = true
 
 
 # If you prefer the indented syntax, you might want to regenerate this

3f26153838386c8b5e5cab289d3c947aa1e8e2aa
Robin Rendle <robinjrendle@gmail.com>
2014-01-14 14:33:15 +0000

    Fixed the lightbox and added a zoom button to the gallery images

--- public/static/scripts/libs/widgets/lightbox.js
+++ public/static/scripts/libs/widgets/lightbox.js
@@ -16,16 +16,16 @@ define(['domReady', 'jquery'], function(domReady, jQuery) {
     domReady(function(){
         var size = window.getComputedStyle(document.documentElement,':after').getPropertyValue('content'); //http://adactio.com/journal/5429/
 
-        console.log('size');
-
         lightboxInit();
+        preventDefault();
+
 
         function lightboxInit() {
-            $('.carousel__link').click(function(e){
+            $('.carousel__zoom').click(function(e) {
                 if(size == 'medium' || size == 'large' || size == 'largest') {
                     e.preventDefault();
                     var $thisHref = $(this).attr('href');
-                    var $thisSummary = $(this).parent().parent().children('.carousel__summary').html();
+                    var $thisSummary = $(this).parent().children('.carousel__summary').html();
                     buildLightBox($thisHref, $thisSummary);
                 }
             });
@@ -41,7 +41,7 @@ define(['domReady', 'jquery'], function(domReady, jQuery) {
             $('<img class="lightbox__image" src="'+src+'" alt="" />').appendTo('.lightbox__container');
 
             // Makes a close button
-            $('<div class="lightbox__close btn btn--icon btn--secondary btn--arrow-left btn--gamma"><span class="icon ss ss-close">close</span><div class="icon-text">Close</div></div>').appendTo('.lightbox__container');
+            $('<div class="lightbox__close btn btn--icon btn--tertiary btn--arrow-left btn--epsilon"><span class="icon ss ss-close">close</span><div class="icon-text">Close</div></div>').appendTo('.lightbox__container');
 
             // Gets the summary and adds it to the lightbox
             $('<div class="lightbox__content">').appendTo('.lightbox__container').html(summary);
@@ -58,6 +58,7 @@ define(['domReady', 'jquery'], function(domReady, jQuery) {
                 }
             });
         }
+
 
         $(window).resize(function() {
             size = window.getComputedStyle(document.documentElement,':after').getPropertyValue('content');

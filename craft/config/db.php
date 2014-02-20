987417b43312f1139aaff4971f8fcec2dc59ebe7
Robin Rendle <robinjrendle@gmail.com>
2014-02-20 11:38:40 +0000

    Fixed the 404 page, updated the about page and homepage images

--- craft/config/db.php
+++ craft/config/db.php
@@ -10,7 +10,7 @@
 return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+	'server' => '127.0.0.1',
 
 	// The database username to connect with.
 	'user' => 'root',
@@ -19,7 +19,7 @@ return array(
 	'password' => '',
 
 	// The name of the database to select.
-	'database' => 'simon_campbell',
+	'database' => 'sc_craft',
 
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
